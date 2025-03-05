0a90526869902c2f88b2beb6684bb6ff960777d5
Chars3 <charlesjoliveira65@gmail.com>
2025-03-05 19:49:05 -0300

    Finalizando a landing page

---
+++ app/components/ui/Button.tsx
@@ -0,0 +1,27 @@
+import { cn } from "../../lib/utils";
+
+function Button({
+  children,
+  variant = "primary",
+  ...props
+}: {
+  children: React.ReactNode;
+  variant?: "primary" | "secondary" | "ghost";
+} & React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      {...props}
+      className={cn(
+        "p-3 text-white rounded-xl font-bold whitespace-nowrap hover:opacity-95 disabled:opacity-70",
+        variant === "primary" && "bg-accent-purple",
+        variant === "secondary" && "bg-background-tertiary",
+        variant === "primary" && "border-border-primary",
+        props.className
+      )}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
